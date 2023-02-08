92f99841bb5c2b8df3a655a2bad5844bb1b17ad9
MiguelCid <miguelcidyevenes@gmail.com>
2023-02-08 09:52:32 -0300

    guardando parte 2 del proyecto

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Creacion de pagina web inventario para ingresar productos del carrito desde la pagina web. 
 
-## Tecnologias a ocupar
+## Tecnologias a ocupar :floppy_disk:
    
     Javascript: Node.js de JS
     Bootstrap para la apariencia
     HTML5
 
-## Github
-    https://github.com/Migcid/crud_inventario_node
+## Github 
+    https://github.com/Migcid/crud_inventario_node :+1:
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const express = require('express')
-
+const express = require('express') // (1) se crea la peticion al servidor
+const fs = require('fs'); // (6) importamos el archivo filesystem
 const app = express();
 
 app.get("/", (req, res) =>{
-    res.send("Bienvenido Miguel");
-});
-app.all("*", (req, res) => {
-    res.status(404).send("No encontrado.")
-})
+    res.send("Bienvenido Miguel"); 
+}); 
+    //(2) crear el require con un callback
 
-app.listen(3000, () => console.log("http://localhost:3000"))
+
+app.get("/productos", (req, res) =>{
+    fs.readFile("productos.json", "utf-8", (err, data) => {
+        if(err) return res.status(500).send({code: 500, message: "No se pudo leer la informacion de productos."})
+        let productos = JSON.parse(data);
+        res.json(productos);
+        
+    })
+
+})   
+    // (5) vamos a traer productos para ingresarlos al productos.json con fileSystem y hay que importarlo arriba como const
+
+app.all("*", (req, res) => {
+    res.status(404).sendfile(__dirname + "/404.html") 
+  
+}) 
+    //(4) hay que crear la direccion del html para que lo bra cuando hay un error en la direccion o no lo encuentre.
+
+app.listen(3000, () => console.log("http://localhost:3000")) 
+    //(3) creamos el puerto
